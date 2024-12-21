2335253c8a696aa72391692ed6a4253fe31e5185
eqmvii <coelomate@gmail.com>
2024-12-20 20:04:05 -0500

    terraform change tutorial

--- main.tf
+++ main.tf
@@ -10,13 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  # TODO - change?  
   region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  # how did it get this amp?  
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
